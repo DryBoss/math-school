b890a64ef66ddfd85823a4f789275e5a30eed5f2
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-08-30 17:58:27 +0600

    navigation bar

--- src/components/main/navigation-bar/Navigation-bar.jsx
+++ src/components/main/navigation-bar/Navigation-bar.jsx
@@ -1,7 +1,12 @@
+import mathSchoolLogo from "./../../../assets/math-school-logo.svg";
+import changeLanguageIcon from "./../../../assets/ui-icons/change-language.svg";
+import changeThemeIcon from "./../../../assets/ui-icons/change-theme.svg";
+import styles from "./Navigation-bar.module.css";
+
 function NavigationBar() {
   return (
-    <div className="navigation-bar">
-      <img src="" alt="math school logo" className="ms-logo" />
+    <div className={styles.navigationBar}>
+      <img src={mathSchoolLogo} alt="math school logo" className="ms-logo" />
       <ul className="menu">
         <li className="menu-items">Home</li>
         <li className="menu-items">Learn</li>
@@ -10,11 +15,12 @@ function NavigationBar() {
         <input type="text" />
         <img src="" alt="search" className="search" />
       </div>
-      <div className="language">
-        <img src="" alt="" className="icon" />
-        <p className="title"></p>
-      </div>
-      <img src="" alt="" className="day-night" />
+      <img
+        src={changeLanguageIcon}
+        alt="language"
+        className="change-language"
+      />
+      <img src={changeThemeIcon} alt="theme" className="change-theme" />
       <div className="account"></div>
     </div>
   );
