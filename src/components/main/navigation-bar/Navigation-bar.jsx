d5c5a642c0ec89ce0ca992586dfc70c220341089
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-08-30 20:03:58 +0600

    navigation bar

--- src/components/main/navigation-bar/Navigation-bar.jsx
+++ src/components/main/navigation-bar/Navigation-bar.jsx
@@ -1,27 +1,32 @@
 import mathSchoolLogo from "./../../../assets/math-school-logo.svg";
+import searchIcon from "./../../../assets/ui-icons/search.svg";
 import changeLanguageIcon from "./../../../assets/ui-icons/change-language.svg";
 import changeThemeIcon from "./../../../assets/ui-icons/change-theme.svg";
+import userProfile from "./../../../assets/ui-icons/user-profile.svg";
 import styles from "./Navigation-bar.module.css";
 
 function NavigationBar() {
+  const menuItems = ["Home", "Learn"];
+
   return (
     <div className={styles.navigationBar}>
       <img src={mathSchoolLogo} alt="math school logo" className="ms-logo" />
-      <ul className="menu">
-        <li className="menu-items">Home</li>
-        <li className="menu-items">Learn</li>
-      </ul>
+      <ul className="menu-pc"></ul>
+      <select name="menu-mobile" className="menu-mobile"></select>
       <div className="search-bar">
         <input type="text" />
-        <img src="" alt="search" className="search" />
+        <img src={searchIcon} alt="search" className="search" />
       </div>
+      <div className="math-lab"></div>
       <img
         src={changeLanguageIcon}
         alt="language"
         className="change-language"
       />
       <img src={changeThemeIcon} alt="theme" className="change-theme" />
-      <div className="account"></div>
+      <div className="account">
+        <img src={userProfile} alt="user" className="user" />
+      </div>
     </div>
   );
 }
